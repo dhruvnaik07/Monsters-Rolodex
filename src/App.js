258c76383f5af829b251fdededd888824ac077f0
Dhruv Naik <dhruv3456@gmail.com>
2019-11-23 23:03:17 -0500

    h1 update

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
 
       <div className="App">
 
-        <h1>Monsters Rolodex</h1>
+        <h1 className="title">Monsters Rolodex</h1>
         <Searchbox placeholder='search monsters' handleChange={this.handleChange}/>
 
         <CardList monsters={filteredMonsters}>        
