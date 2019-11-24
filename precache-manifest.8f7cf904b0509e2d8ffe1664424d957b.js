3142d7e8f29575e4bdf9f0753264b8a16058f2be
Dhruv Naik <dhruv3456@gmail.com>
2019-11-23 22:54:43 -0500

    Updates

---
+++ precache-manifest.8f7cf904b0509e2d8ffe1664424d957b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0056ede04c816ddd255956e616925d3f",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "86e8b3865e35a14c2811",
+    "url": "/Monsters-Rolodex/static/css/main.21705408.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/Monsters-Rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "86e8b3865e35a14c2811",
+    "url": "/Monsters-Rolodex/static/js/main.d9a035d9.chunk.js"
+  },
+  {
+    "revision": "9878fa4107f8356aba7e",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.e3ed63f9.js"
+  }
+]);
